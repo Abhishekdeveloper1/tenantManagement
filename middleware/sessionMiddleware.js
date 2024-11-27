f2c4e2a91f19f6c86312ac549f1ca85961602fcc
Abhishekdeveloper1 <abhicodergenx@gmail.com>
2024-11-27 22:09:08 +0530

    update2711202402

--- middleware/sessionMiddleware.js
+++ middleware/sessionMiddleware.js
@@ -2,7 +2,7 @@ const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
 const sessionMiddleware = session({
-  secret: 'yourSecretKey',
+  secret: 'abhishek123411o100!223',
   resave: false,           
   saveUninitialized: false,
   cookie: { maxAge: 60000 * 60 * 24 },
