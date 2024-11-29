62d6ced210b67d730157b51d08506d5b4c261760
Abhishekdeveloper1 <abhicodergenx@gmail.com>
2024-11-29 19:46:59 +0530

    update2911202404

--- routes/property.js
+++ routes/property.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const { propertyDetails } = require("../controllers/auth/PropertyController");
+const { propertyDetails } = require("../controllers/PropertyController");
 router.get('/property-details',propertyDetails);
 
 module.exports=router;
